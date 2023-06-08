319d6c6accd5f9db2f81dbbdd7bbc6ce9d5b72f5
Andrea <andrea.sualo@gmail.com>
2023-06-08 17:04:26 +0200

    feat: create the model, migrations and seeders of Role

---
+++ seeders/20230608145505-roles.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Roles', [
+      {
+        id: 1,
+        name: 'patient',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 2,
+        name: 'doctor',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 3,
+        name: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
