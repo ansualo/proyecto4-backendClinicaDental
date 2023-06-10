25acf1bfe100d28f09188b62fce78bffb5c7403f
Andrea <andrea.sualo@gmail.com>
2023-06-10 20:54:00 +0200

    feat: updateAppointment and deleteAppointment done

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,4 +1,5 @@
-const { Appointment, User } = require('../models')
+const { Appointment, User } = require('../models');
+const appointment = require('../models/appointment');
 
 const appointmentController = {};
 
@@ -49,8 +50,121 @@ appointmentController.createAppointment = async (req, res) => {
     }
 }
 
+appointmentController.updateAppointment = async (req, res) => {
+    try {
 
+        const patientId = req.userId
+        const appointmentId = req.params.id
+        const { user_id_2, treatment_id, date } = req.body
 
+        // buscamos la cita en la base de datos
+        const appointment = await Appointment.findByPk(appointmentId)
+
+        // comprobamos que la cita pertenece al paciente
+        if (appointment.user_id_1 !== patientId){
+            return res.json(
+                {
+                    success: true,
+                    message: "Incorrect appointment"
+                }
+            )
+        }
+
+        // buscamos el dentista en la base de datos
+        const dentist = await User.findByPk(user_id_2);
+
+        //comprobamos que el rol es el de dentista
+        if(dentist.role_id !== 2){ 
+            return res.json(
+                {
+                    success: true,
+                    message: "Incorrect doctor",
+                }
+            )
+        };
+
+        updatedAppointment = await Appointment.update(
+            {
+                user_id_2: user_id_2,
+                treatment_id: treatment_id,
+                date: date,
+            },
+            {
+                where: {
+                    id: appointmentId
+                }
+            }
+        )
+        const newAppointment = await Appointment.findByPk(
+            appointmentId,
+            {
+                attributes: {
+                    exclude: ["createdAt", "updatedAt"]
+                },
+            }
+        );
+
+        return res.json(
+            {
+                success: true,
+                message: "Appointment updated successfully",
+                data: newAppointment
+            }
+        )
+      
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: "Appointment cannot be updated",
+                error: error.message
+            }
+        )
+    }
+}
+
+appointmentController.deleteAppointment = async(req, res) => {
+    try {
+
+        const patientId = req.userId
+        const appointmentId = req.params.id
+
+        // buscamos la cita en la base de datos
+        const appointment = await Appointment.findByPk(appointmentId)
+
+        // comprobamos que la cita pertenece al paciente
+        if (appointment.user_id_1 !== patientId){
+            return res.json(
+                {
+                    success: true,
+                    message: "Incorrect appointment"
+                }
+            )
+        }
+
+        const deletedAppointment = await Appointment.destroy({
+            where: {
+                id: appointmentId
+            }
+        })
+        
+        return res.json(
+            {
+                success: true,
+                message: "Appointment deleted successfully",
+                data: deletedAppointment
+            }
+        );
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: "Appointment cannot be deleted",
+                error: error.message
+            }
+        )
+    }
+}
 
 
 
--- views/appointmentRoutes.js
+++ views/appointmentRoutes.js
@@ -6,6 +6,8 @@ const auth = require('../middlewares/verifyToken');
 
 
 router.post('/', auth, appointmentController.createAppointment);
+router.put('/:id', auth, appointmentController.updateAppointment);
+router.delete('/:id', auth, appointmentController.deleteAppointment);
 
 
 module.exports = router;
